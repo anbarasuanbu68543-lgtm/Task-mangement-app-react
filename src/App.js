714087eb511ae34c93b08f8c4d1d7ffc693c2aea
anbarasuanbu68543-lgtm <anbarasuanbu68543@gmail.com>
2025-11-13 22:24:50 +0530

    updated all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Forrm from "./components/Form";
+import Table from "./components/Table";
 function App() {
+  const initialFormData = {
+    name: "",
+    email: "",
+  };
+  const [formData, setformData] = useState(initialFormData);
+  const [tableData, setTableData] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const handleFormDataChange = (key, value) => {
+    console.log({ key, value });
+    setformData({
+      ...formData,
+      [key]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log("Form submitted:", formData);
+    if (editIndex == null) {
+      setTableData([...tableData, formData]);
+      setformData(initialFormData);
+    } else {
+      tableData[editIndex] = formData;
+      setformData(initialFormData);
+      setTableData(tableData);
+      setEditIndex(null);
+    }
+  };
+  const handleEdit = (index) => {
+    console.log("Edit clicked on row", index);
+    const clickItem = tableData[index];
+    setformData(clickItem);
+    setEditIndex(index);
+  };
+  const handleDelte = (index) => {
+    tableData.splice(index, 1);
+    setTableData([...tableData]);
+  };
+  // console.log("form data change", formData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-6 mx-auto">
+      <Forrm
+        formdata={formData}
+        handleFormDataChange={handleFormDataChange}
+        handleSubmit={handleSubmit}
+        editIndex={editIndex}
+      />
+      <Table
+        tableData={tableData}
+        handleEdit={handleEdit}
+        handleDelte={handleDelte}
+      />
     </div>
   );
 }
